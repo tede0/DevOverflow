328f29c23e39b945343c22ac974dc22d181ce0f8
mlv02 <maksym.lyskov@sky.uk>
2023-10-13 14:58:54 +0100

    theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import {ThemeProvider} from '@/context/ThemeProvider'
 import {ClerkProvider} from '@clerk/nextjs'
 import type {Metadata} from 'next'
 import {Inter, Space_Grotesk} from 'next/font/google'
@@ -27,12 +28,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <ClerkProvider
-      appearance={{elements: {formButtonPrimary: 'primary-gradient', footerActionLink: 'primary-text-gradient hover:text-primary-500'}}}
-    >
-      <html lang='en'>
-        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>{children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang='en'>
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{elements: {formButtonPrimary: 'primary-gradient', footerActionLink: 'primary-text-gradient hover:text-primary-500'}}}
+        >
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   )
 }
