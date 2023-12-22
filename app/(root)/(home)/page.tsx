3d1e8de2eb1e90461498d6d3dee175044f3bb4aa
Maksym Lyskov <admin@Maksyms-MBP.lan>
2023-12-22 16:40:10 +0000

    added vscode settings to the project

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,39 +1,66 @@
-import HomeFilters from '@/components/home/HomeFilters'
-import QuestionCard from '@/components/home/QuestionCard'
-import Filter from '@/components/shared/Filter'
-import LocalSearchBar from '@/components/shared/search/LocalSearchBar'
-import {Button} from '@/components/ui/button'
-import {HomePageFilters} from '@/constants/filters'
-import Link from 'next/link'
+import HomeFilters from "@/components/home/HomeFilters";
+import QuestionCard from "@/components/home/QuestionCard";
+import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
+import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
+import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
+import Link from "next/link";
+
+const questions = [
+  {
+    _id: 1,
+    title: "How to center a div?",
+    tags: [{ _id: 1, name: "css" }],
+    author: "John Doe",
+    upvotes: 10,
+    views: 100,
+    answers: 2,
+    createdAt: "2021-09-01T12:00:00.000Z",
+  },
+];
 
 export default function Home() {
   return (
     <>
-      <div className='flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center'>
-        <h1 className='h1-bold text-dark100_light900'>All Questions</h1>
-        <Link className='flex justify-end max-sm:w-full' href={'/ask-question'}>
-          <Button className='primary-gradient min-h-[46px] px-4 py-3 !text-light-900'>Ask a Questions</Button>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Link className="flex justify-end max-sm:w-full" href={"/ask-question"}>
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            Ask a Questions
+          </Button>
         </Link>
       </div>
 
-      <div className='mt-11 flex justify-between gap-5 max-sm:flex-col'>
-        <LocalSearchBar route='/' />
-        <Filter filters={HomePageFilters} otherClasses='min-h-[56px] sm:min-w-[170px]' containerClasses='hidden max-md:flex' />
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col">
+        <LocalSearchBar route="/" />
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
       </div>
 
       <HomeFilters />
 
-      <div className='mt-11'>
-        <QuestionCard
-          title='Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?'
-          tags={['REACT.JS', 'NEXT.JS']}
-          author='Sujata | JS Mastery'
-          dateAsked={new Date(2022, 4, 17)}
-          numberOfVotes={67}
-          numberOfAnswers={51}
-          numberOfViews={3967}
-        />
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              createdAt={new Date(question.createdAt)}
+              upvotes={question.upvotes}
+              answers={question.answers}
+              views={question.views}
+            />
+          ))
+        ) : (
+          <NoResult />
+        )}
       </div>
     </>
-  )
+  );
 }
--- components/home/QuestionCard.tsx
+++ components/home/QuestionCard.tsx
@@ -1,45 +1,70 @@
-import Image from 'next/image'
-import React from 'react'
-import {Button} from '../ui/button'
+import Image from "next/image";
+import React from "react";
+import { Button } from "../ui/button";
 
 interface QuestionCardProps {
-  title: string
-  tags: string[]
-  author: string
-  dateAsked: Date
-  numberOfVotes: number
-  numberOfAnswers: number
-  numberOfViews: number
+  title: string;
+  tags: { _id: number; name: string }[];
+  author: string;
+  createdAt: Date;
+  upvotes: number;
+  answers: number;
+  views: number;
 }
 
-const QuestionCard = ({title, tags, author, dateAsked, numberOfVotes, numberOfAnswers, numberOfViews}: QuestionCardProps) => {
+const QuestionCard = ({
+  title,
+  tags,
+  author,
+  createdAt: dateAsked,
+  upvotes: numberOfVotes,
+  answers: numberOfAnswers,
+  views: numberOfViews,
+}: QuestionCardProps) => {
   return (
-    <div className='background-light800_darkgradient rounded-lg p-8'>
-      <div className='line-clamp-1 text-lg font-bold dark:text-white'>{title}</div>
-      <div className='mt-3 flex gap-3'>
+    <div className="background-light800_darkgradient rounded-lg p-8">
+      <div className="line-clamp-1 text-lg font-bold dark:text-white">
+        {title}
+      </div>
+      <div className="mt-3 flex gap-3">
         {tags.map((tag, index) => (
           <Button
             key={index}
-            className='body-medium text-xs px-6 py-3 bg-light-800 dark:bg-dark-300 text-light-500 rounded-lg capitalize shadow-none'
+            className="body-medium rounded-lg bg-light-800 px-6 py-3 text-xs capitalize text-light-500 shadow-none dark:bg-dark-300"
           >
-            {tag}
+            {tag.name}
           </Button>
         ))}
       </div>
 
-      <div className='flex justify-between mt-5 text-sm dark:text-white'>
+      <div className="mt-5 flex justify-between text-sm dark:text-white">
         {author} | {dateAsked.toUTCString()}
-        <div className='flex items-center gap-1'>
-          <Image src='/assets/icons/like.svg' alt='upvote' width={16} height={16} />
-          <div className='text-xs'>{numberOfVotes} Votes</div>
-          <Image src='/assets/icons/message.svg' alt='upvote' width={16} height={16} />
-          <div className='text-xs'>{numberOfAnswers} Comments</div>
-          <Image src='/assets/icons/eye.svg' alt='upvote' width={16} height={16} />
-          <div className='text-xs'>{numberOfViews} Views</div>
+        <div className="flex items-center gap-1">
+          <Image
+            src="/assets/icons/like.svg"
+            alt="upvote"
+            width={16}
+            height={16}
+          />
+          <div className="text-xs">{numberOfVotes} Votes</div>
+          <Image
+            src="/assets/icons/message.svg"
+            alt="upvote"
+            width={16}
+            height={16}
+          />
+          <div className="text-xs">{numberOfAnswers} Comments</div>
+          <Image
+            src="/assets/icons/eye.svg"
+            alt="upvote"
+            width={16}
+            height={16}
+          />
+          <div className="text-xs">{numberOfViews} Views</div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default QuestionCard
+export default QuestionCard;
